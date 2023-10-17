cc4b3d9a837876f7fe7afae06b7da253828f3c03
Proz <joaovitorsoaresdosreis3@gmail.com>
2023-10-17 14:30:52 -0300

    Pegando o arquivo base

---
+++ Atividades05/Exe01.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE BDEscolar;
+USE BDEscolar;
+
+CREATE TABLE Professores (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Turmas (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NomeTurma VARCHAR(20) NOT NULL,
+    ProfessorID INT,
+    FOREIGN KEY (ProfessorID) REFERENCES Professores(ID)
+);
+
+CREATE TABLE Alunos (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(50) NOT NULL,
+    DataNascimento DATE,
+    TurmaID INT,
+    FOREIGN KEY (TurmaID) REFERENCES Turmas(ID)
+);
+
+CREATE TABLE Disciplinas (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NomeDisciplina VARCHAR(30) NOT NULL,
+    TurmaID INT,
+    FOREIGN KEY (TurmaID) REFERENCES Turmas(ID)
+);
+
+CREATE TABLE UnidadesEscolares (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NomeUnidade VARCHAR(50) NOT NULL,
+    Endereco VARCHAR(100)
+);
+
